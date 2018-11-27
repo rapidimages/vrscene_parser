7bdd7be8ff1f002e59825cd8ac99e77ba4d7b317
Martin Nilsson <martin.lennart.nilsson@gmail.com>
2018-11-27 11:37:13 +0100

    Cleanup the parser.

--- main.cpp
+++ main.cpp
@@ -9,32 +9,34 @@ static auto current_time() {
     return duration_cast<microseconds>(system_clock::now().time_since_epoch());
 }
 
-int main(int, char**) {
-    std::ifstream ifs ("../merged.vrscene", std::ifstream::in);
+int main(int, char** argv) {
+    auto file_start = current_time();
+    std::ifstream ifs (argv[1], std::ifstream::in);
     std::string str((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-    std::string_view string(str);
     ifs.close();
+    auto file_dur = current_time() - file_start;
+    auto parse_start = current_time();
 
-    auto start = current_time();
-    if(auto scene = parse_vrscene(string); scene) {
+    if(auto scene = vrp::parse_vrscene(str); scene) {
         auto vrscene = *scene;
-        auto dur = current_time() - start;
+        auto parse_dur = current_time() - parse_start;
 
-        std::cout << "SUCCESS " << dur.count() << std::endl;
-        auto i = 0;
+        std::cout << "SUCCESS " << std::endl;
+        std::cout << "File reading: " << (file_dur.count() / 1000.0f) << " ms\n";
+        std::cout << "Parsing: " << (parse_dur.count() / 1000.0f) << " ms\n";
+
         for(auto&& c : vrscene.comments) {
             std::cout << c;
         }
+
         for(auto&& i : vrscene.includes) {
             std::cout << i;
         }
 
+        auto i = 0;
         for(auto&& p : vrscene.plugins) {
             if(i++ >= 10) break;
-            std::cout << p.type << " " << p.name << std::endl;
-            for(auto&& it : p.attributes) {
-                std::cout << "\t" << it.first << "=" << it.second << std::endl;
-            }
+            std::cout << p << std::endl;
         }
     }
     return 0;
